de70c6ad438fbc256bd3b9494568d3684518d1f7
Mohamad Bashyr <mbashirkuja@gmail.com>
2026-06-26 21:57:03 +0300

    edited

--- app/index.jsx
+++ app/index.jsx
@@ -1,8 +1,10 @@
+import { useRouter } from "expo-router";
 import { useState } from "react";
 import { Image, Pressable, Text, View } from "react-native";
 
 export default function Index() {
   const [num, setNum] = useState(0);
+  const router = useRouter();
   return (
     <View className="flex-1 items-center  bg-gray-800">
       <View className="w-full h-1/2 px-1">
@@ -15,7 +17,7 @@ export default function Index() {
       <Text className="text-xl font-thin text-gray-400 mt-4 text-center w-screen px-8">
         Your ultimate study companion for effective learning and productivity.
       </Text>
-      <Pressable onPress={() => setNum(num + 1)} className="bg-blue-600 px-6 py-4 rounded-full absolute bottom-12 w-10/12 items-center">
+      <Pressable onPress={() => router.push("/subject")} className="bg-blue-600 px-6 py-4 rounded-full absolute bottom-12 w-10/12 items-center">
         <Text className="text-white">
           I,m pressable
         </Text>
---
+++ app/subject.jsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+export default function Subject() {
+    return (
+        <>
+        <View className="flex-1 items-center justify-center bg-gray-800">
+            <Text className="text-white text-2xl font-bold">
+                Subject Screen
+            </Text>
+        </View>
+        </>
+    );
+}
