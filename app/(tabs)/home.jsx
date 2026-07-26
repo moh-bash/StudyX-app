4f9612007c87008954adec0c3d28af75a68499fe
Mohamad Bashyr <mbashirkuja@gmail.com>
2026-07-26 21:53:57 +0300

    k

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,10 +1,13 @@
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function home() {
   return (
     <SafeAreaView className="flex-1 justify-center items-center bg-gray-800 p-4">
       <Text className="text-white text-2xl font-bold">welcome to Home</Text>
+      <View className="mt-4 p-4 bg-gray-700 rounded-lg">
+        <Text className="text-white text-lg">This is a sample home screen.</Text>
+      </View>
     </SafeAreaView>
   )
 }
